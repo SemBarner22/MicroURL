60162618e14e0617fe41271bdf549369229818ec
Ildar Zagretdinov <sembarner22@gmail.com>
2023-09-01 17:24:01 +0200

    Initial commit with ping controller

---
+++ src/main/java/com/micro/url/MicroURLApplication.java
@@ -0,0 +1,13 @@
+package com.micro.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroURLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroURLApplication.class, args);
+    }
+
+}
